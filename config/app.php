b6a6ffd293a16f864cb25eb5d472f6a6ed74de77
Timothy Andrew Gass <timothyagass@gmail.com>
2016-03-24 00:14:54 -0500

    defective

--- config/app.php
+++ config/app.php
@@ -153,9 +153,7 @@ return [
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
-
-        Shpasser\GaeSupportL5\GaeSupportServiceProvider::class
+        App\Providers\RouteServiceProvider::class
     ],
 
     /*
